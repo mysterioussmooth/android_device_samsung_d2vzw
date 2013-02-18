a96bd30674dc4c04eaa991c6bd5945fc3511788c
liquid0624 <rjbirnesser@gmail.com>
2013-02-18 14:25:37 -0500

    Update Build Fingerprint VRBMA2 (Credit Invisiblek)

--- liquid.mk
+++ liquid.mk
@@ -34,10 +34,10 @@ PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=liquid.d2vzw.$(shell date +%m%d%y).$
 
 # override
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_NUMBER=I535VRBLK3 \
+    BUILD_NUMBER=I535VRBMA2 \
     PRODUCT_NAME=d2vzw \
     TARGET_DEVICE=d2vzw \
     TARGET_BUILD_TYPE=user \
     BUILD_VERSION_TAGS=release-keys \
-    PRIVATE_BUILD_DESC="d2vzw-user 4.1.1 JRO03L I535VRBLK3 release-keys" \
-    BUILD_FINGERPRINT="Verizon/d2vzw/d2vzw:4.1.1/JRO03L/I535VRBLK3:user/release-keys"
+    PRIVATE_BUILD_DESC="d2vzw-user 4.1.2 JZO54K I535VRBMA2 release-keys" \
+    BUILD_FINGERPRINT="Verizon/d2vzw/d2vzw:4.1.2/JZO54K/I535VRBMA2:user/release-keys"
